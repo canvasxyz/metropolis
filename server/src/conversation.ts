c87b5823372a39248f83c4db927e4c6ca9fd09e1
Bob Webb <bob.wbb@gmail.com>
2023-09-25 20:47:12 +0100

    remove callbacks from conversation.ts

--- server/src/conversation.ts
+++ server/src/conversation.ts
@@ -26,7 +26,7 @@ function createXidRecord(
   );
 }
 
-function createXidRecordByZid(
+async function createXidRecordByZid(
   zid: any,
   uid: any,
   xid: any,
@@ -34,29 +34,28 @@ function createXidRecordByZid(
   x_name: any,
   x_email: any
 ) {
-  return getConversationInfo(zid).then(
-    (conv: { use_xid_whitelist: any; owner: any }) => {
-      const shouldCreateXidRecord = conv.use_xid_whitelist
-        ? isXidWhitelisted(conv.owner, xid)
-        : Promise.resolve(true);
-      return shouldCreateXidRecord.then((should: any) => {
-        if (!should) {
-          throw new Error("polis_err_xid_not_whitelisted_2");
-        }
-        return pg.queryP(
-          "insert into xids (owner, uid, xid, x_profile_image_url, x_name, x_email) values ((select org_id from conversations where zid = ($1)), $2, $3, $4, $5, $6) " +
-            "on conflict (owner, xid) do nothing;",
-          [
-            zid,
-            uid,
-            xid,
-            x_profile_image_url || null,
-            x_name || null,
-            x_email || null,
-          ]
-        );
-      });
-    }
+  const conv = await getConversationInfo(zid) as { use_xid_whitelist: any; owner: any };
+
+  const shouldCreateXidRecord = conv.use_xid_whitelist
+    ? isXidWhitelisted(conv.owner, xid)
+    : Promise.resolve(true);
+
+  const should = await shouldCreateXidRecord;
+
+  if (!should) {
+    throw new Error("polis_err_xid_not_whitelisted_2");
+  }
+  return pg.queryP(
+    "insert into xids (owner, uid, xid, x_profile_image_url, x_name, x_email) values ((select org_id from conversations where zid = ($1)), $2, $3, $4, $5, $6) " +
+      "on conflict (owner, xid) do nothing;",
+    [
+      zid,
+      uid,
+      xid,
+      x_profile_image_url || null,
+      x_name || null,
+      x_email || null,
+    ]
   );
 }
 
@@ -85,48 +84,24 @@ function isXidWhitelisted(owner: any, xid: any) {
   );
 }
 
-function getConversationInfo(zid: any) {
-  //   (alias) function MPromise(name: string, f: (resolve: (value: unknown) => void, reject: (reason?: any) => void) => void): Promise<unknown>
-  // import MPromise
-  // 'new' expression, whose target lacks a construct signature, implicitly has an 'any' type.ts(7009)
-  // @ts-ignore
-  return new MPromise(
+async function getConversationInfo(zid: any) {
+  return MPromise(
     "getConversationInfo",
-    function (resolve: (arg0: any) => void, reject: (arg0: any) => void) {
-      pg.query(
-        "SELECT * FROM conversations WHERE zid = ($1);",
-        [zid],
-        function (err: any, result: { rows: any[] }) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result.rows[0]);
-          }
-        }
-      );
+    async () => {
+      const {rows} = await pg.queryP("SELECT * FROM conversations WHERE zid = ($1);", [zid]) as {rows: any[]};
+      return rows[0];
     }
   );
 }
 
 function getConversationInfoByConversationId(conversation_id: any) {
-  //   (alias) function MPromise(name: string, f: (resolve: (value: unknown) => void, reject: (reason?: any) => void) => void): Promise<unknown>
-  // import MPromise
-  // 'new' expression, whose target lacks a construct signature, implicitly has an 'any' type.ts(7009)
-  // @ts-ignore
-  return new MPromise(
+  return MPromise(
     "getConversationInfoByConversationId",
-    function (resolve: (arg0: any) => void, reject: (arg0: any) => void) {
-      pg.query(
+    async () => {
+      const {rows} = await pg.queryP(
         "SELECT * FROM conversations WHERE zid = (select zid from zinvites where zinvite = ($1));",
-        [conversation_id],
-        function (err: any, result: { rows: any[] }) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result.rows[0]);
-          }
-        }
-      );
+        [conversation_id]) as { rows: any[] };
+      return rows[0];
     }
   );
 }
@@ -137,37 +112,28 @@ const conversationIdToZidCache = new LruCache({
 
 // NOTE: currently conversation_id is stored as zinvite
 function getZidFromConversationId(conversation_id: string) {
-  //   (alias) function MPromise(name: string, f: (resolve: (value: unknown) => void, reject: (reason?: any) => void) => void): Promise<unknown>
-  // import MPromise
-  // 'new' expression, whose target lacks a construct signature, implicitly has an 'any' type.ts(7009)
-  // @ts-ignore
-  return new MPromise(
+  return MPromise(
     "getZidFromConversationId",
-    function (resolve: (arg0: any) => void, reject: (arg0: string) => any) {
+    async () => {
       let cachedZid = conversationIdToZidCache.get(conversation_id);
       if (cachedZid) {
-        resolve(cachedZid);
-        return;
+        return cachedZid;
       }
-      pg.query_readOnly(
+
+      const results = await pg.queryP_readOnly(
         "select zid from zinvites where zinvite = ($1);",
-        [conversation_id],
-        function (err: any, results: { rows: string | any[] }) {
-          if (err) {
-            return reject(err);
-          } else if (!results || !results.rows || !results.rows.length) {
-            logger.error(
-              "polis_err_fetching_zid_for_conversation_id " + conversation_id,
-              err
-            );
-            return reject("polis_err_fetching_zid_for_conversation_id");
-          } else {
-            let zid = results.rows[0].zid;
-            conversationIdToZidCache.set(conversation_id, zid);
-            return resolve(zid);
-          }
-        }
-      );
+        [conversation_id]) as { rows: string | any[] };
+
+      if (!results || !results.rows || !results.rows.length) {
+        logger.error(
+          "polis_err_fetching_zid_for_conversation_id " + conversation_id
+        );
+        throw Error("polis_err_fetching_zid_for_conversation_id");
+      } else {
+        let zid = results.rows[0].zid;
+        conversationIdToZidCache.set(conversation_id, zid);
+        return zid;
+      }
     }
   );
 }
