5453822924f5d72ff0a13a8c5dcbc293c6165922
Bob Webb <bob.wbb@gmail.com>
2023-10-02 14:29:58 +0100

    Frontmatter should fall back to displaying yaml code if it can't parse

--- client/src/pages/Frontmatter.tsx
+++ client/src/pages/Frontmatter.tsx
@@ -7,28 +7,55 @@ import { Box } from "theme-ui"
 type FrontmatterProps = {source: string}
 
 const useParsedFrontmatter = (source: string) => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<Error|null>(null)
+
   const [frontmatter, setFrontmatter] = useState<Record<string,any>|null>(null)
 
   useEffect(() => {
-    const proc = remark().use(remarkFrontMatter, {type: "yaml", marker: "-"}).use(remarkParseFrontmatter as any)
-    const output = proc.processSync(source)
+    // @ts-ignore
+    const proc = remark().use(remarkFrontMatter, {type: "yaml", marker: "-"}).use(remarkParseFrontmatter)
 
-    setFrontmatter(output.data.frontmatter)
+    proc.process(source).then(output => {
+
+      setFrontmatter(output.data.frontmatter)
+      setLoading(false)
+    }).catch(err => {
+      setError(err)
+      setLoading(false)
+    })
+
 
   }, [source])
 
-  return {frontmatter}
+  return {frontmatter, loading, error}
 }
 
 export const Frontmatter: React.FC<FrontmatterProps> = ({source}) => {
-  const {frontmatter} = useParsedFrontmatter(source)
+  const {frontmatter, loading, error} = useParsedFrontmatter(source)
 
-  if(frontmatter == null) return <></>
+  if (loading) {
+    return <Box sx={{ my: [3], px: [3], py: [3], border: "1px solid #ddd" }}>
+      Loading...
+    </Box>
+  }
+
+  if (error) {
+    return <Box sx={{ overflowX: "scroll", my: [3], px: [3], py: [3], border: "1px solid #ddd" }}>
+      {error.message}
+      <pre>{source.split("---")[1]}</pre>
+    </Box>
+  }
+  if (frontmatter === null) {
+    return <Box sx={{ my: [3], px: [3], py: [3], border: "1px solid #ddd" }}>
+      No frontmatter
+    </Box>
+  }
 
   return  (
     <Box sx={{ my: [3], px: [3], py: [3], border: "1px solid #ddd" }}>
       {Object.entries(frontmatter).map(([key, value]) =>
-        <Box key={key}> {key}: {value}</Box>
+        <Box key={key}> {key}: {JSON.stringify(value)}</Box>
       )}
     </Box>
   )
