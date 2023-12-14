2dd60567362e0f1abaed13cf9baa5fd6945cc78c
Bob Webb <bob.wbb@gmail.com>
2023-12-14 14:48:24 +0100

    remove unused FB namespace from client

--- client/src/globals.d.ts
+++ client/src/globals.d.ts
@@ -3,11 +3,6 @@ import { ThemeUIStyleObject } from "theme-ui"
 
 declare module "*.md"
 
-declare namespace FB {
-  const api: any
-  const login: any
-}
-
 declare module "react" {
   interface Attributes {
     sx?: ThemeUIStyleObject
