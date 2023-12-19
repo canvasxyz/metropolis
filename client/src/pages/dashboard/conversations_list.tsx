546cd35edf9536f37b16832f231b9c11ee73faf7
Bob Webb <bob.wbb@gmail.com>
2023-12-19 11:48:11 +0100

    add children type to Badge

--- client/src/pages/dashboard/conversations_list.tsx
+++ client/src/pages/dashboard/conversations_list.tsx
@@ -7,7 +7,7 @@ import { RootState } from "../../store"
 import { ConversationSummary, populateConversationsSummary } from "../../reducers/conversations_summary"
 import { useHistory } from "react-router-dom"
 
-const Badge = ({ children }) => {
+const Badge = ({ children }: { children: React.ReactNode }) => {
   return (
     <Box
       sx={{
