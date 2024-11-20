bc9ed16540582404e4e36645cb9241d180ba86db
Bob Webb <bob.wbb@gmail.com>
2024-11-20 17:03:01 +0100

    fix heading

--- client/src/pages/dashboard/sentiment_checks/index.tsx
+++ client/src/pages/dashboard/sentiment_checks/index.tsx
@@ -109,7 +109,7 @@ export default () => {
         gap: [3],
       }}
     >
-      <Text sx={{ fontWeight: 600, fontSize: [2] }}>FIP Tracker</Text>
+      <Text sx={{ fontWeight: 600, fontSize: [2] }}>Sentiment Checks</Text>
       <Flex sx={{ gap: [2], width: "100%" }}>
         <Box sx={{ flexGrow: "1", maxWidth: "400px" }}>
           <TextField.Root
