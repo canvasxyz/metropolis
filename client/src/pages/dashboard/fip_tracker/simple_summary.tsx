6baf03e33772a36d8f7f9cabe3a2d3ea00c59a82
Bob Webb <bob.wbb@gmail.com>
2024-12-04 15:00:57 +0100

    if simple summary or summary are not found, extract first section

--- client/src/pages/dashboard/fip_tracker/simple_summary.tsx
+++ client/src/pages/dashboard/fip_tracker/simple_summary.tsx
@@ -4,6 +4,7 @@ import remarkGfm from "remark-gfm"
 import markdown from "remark-parse"
 import { remark } from "remark"
 import { unified } from "unified"
+import { Box } from "theme-ui"
 
 export const extractParagraphByTitle = (markdownText, title) => {
   const tree = unified().use(markdown).parse(markdownText)
@@ -44,6 +45,32 @@ export const extractParagraphByTitle = (markdownText, title) => {
   return extractedNodes.map(node => remark().stringify(node)).join("\n")
 }
 
+export const extractFirstSection = (markdownText) => {
+  const tree = unified().use(markdown).parse(markdownText)
+
+  let titleDepth: number | null = null
+  let foundHeading = false
+  const extractedNodes: typeof tree.children = []
+
+  for(const node of tree.children) {
+    if(foundHeading) {
+      if (node.type === "heading" && node.depth <= titleDepth) {
+        // Stop if an equally or more important heading is found
+        break
+      }
+      extractedNodes.push(node)
+    } else {
+      if (node.type === "heading") {
+          foundHeading = true
+          titleDepth = node.depth
+      }
+    }
+  }
+
+  return extractedNodes.map(node => remark().stringify(node)).join("\n")
+}
+
+
 type SimpleSummaryProps = {
   content: string
 }
@@ -60,19 +87,27 @@ const SimpleSummary = ({ content }: SimpleSummaryProps) => {
     if (summaryContent)  {
       return { title: "Summary", content: summaryContent }
     }
+
+    const firstSectionContent = extractFirstSection(content)
+    if (firstSectionContent) {
+      return { title: null, content: firstSectionContent }
+    }
+
   }, [content])
 
   if (extractedData) {
     return <React.Fragment>
-      <h3 style={{ margin: "15px 0 10px" }}>{extractedData.title}</h3>
-      <ReactMarkdown
-        skipHtml={true}
-        remarkPlugins={[remarkGfm]}
-        linkTarget="_blank"
-        className="react-markdown"
-      >
-        {extractedData.content}
-      </ReactMarkdown>
+      {extractedData.title !== null && <h3>{extractedData.title}</h3>}
+      <Box sx={{mt: [2]}}>
+        <ReactMarkdown
+          skipHtml={true}
+          remarkPlugins={[remarkGfm]}
+          linkTarget="_blank"
+          className="react-markdown"
+        >
+          {extractedData.content}
+        </ReactMarkdown>
+      </Box>
     </React.Fragment>
   } else {
     return <React.Fragment>
