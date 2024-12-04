c8a18cc4c09abfda77fefcd581e9e6bc33d8f245
Bob Webb <rjwebb@users.noreply.github.com>
2024-12-04 15:51:29 +0100

    Handle when Simple Summary has multiple paragraphs (#70)
    * update simple summary code to return everything between the simple summary heading and the next significant heading
    
    * extract Summary if Simple Summary does not exist, handle cases where header has strong/emphasis/delete node inside
    
    * simplify SimpleSummary component
    
    * if simple summary or summary are not found, extract first section
    
    * client: max width for img
    
    ---------
    
    Co-authored-by: Raymond Z <raykyri@gmail.com>

--- client/src/pages/dashboard/fip_tracker/simple_summary.tsx
+++ client/src/pages/dashboard/fip_tracker/simple_summary.tsx
@@ -4,48 +4,123 @@ import remarkGfm from "remark-gfm"
 import markdown from "remark-parse"
 import { remark } from "remark"
 import { unified } from "unified"
+import { Box } from "theme-ui"
 
 export const extractParagraphByTitle = (markdownText, title) => {
   const tree = unified().use(markdown).parse(markdownText)
 
-  let inDesiredSection = false
-  let extractedParagraph: string | null = null
+  let titleDepth: number | null = null
+  let foundHeading = false
+  const extractedNodes: typeof tree.children = []
 
-  function visitNode(node) {
-    if (node.type === "heading" && node.children[0].value === title) {
-      inDesiredSection = true
-    } else if (inDesiredSection) {
-      if (node.type === "paragraph" || node.type === "list") {
-        extractedParagraph = remark().stringify(node)
-        inDesiredSection = false // Stop after finding the first paragraph
-      } else if (node.type === "heading" && node.depth <= 2) {
-        inDesiredSection = false // Stop if another heading of depth 1 or 2 is found
+  for(const node of tree.children) {
+    if(foundHeading) {
+      if (node.type === "heading" && node.depth <= titleDepth) {
+        // Stop if an equally or more important heading is found
+        break
+      }
+      extractedNodes.push(node)
+    } else {
+      if (node.type === "heading") {
+        let hasTitle = false
+        const child = node.children[0]
+        if (child.type === "text") {
+          if(child.value === title) {
+            hasTitle = true
+          }
+        } else if (child.type === "strong" || child.type === "emphasis" || child.type === "delete" ) {
+          const grandChild = child.children[0]
+          if(grandChild.type === "text" && grandChild.value === title) {
+            hasTitle = true
+          }
+        }
+        if (hasTitle) {
+          foundHeading = true
+          titleDepth = node.depth
+        }
       }
     }
   }
 
-  tree.children.forEach(visitNode)
-
-  return extractedParagraph
+  return extractedNodes.map(node => remark().stringify(node)).join("\n")
 }
+
+export const extractFirstSection = (markdownText) => {
+  const tree = unified().use(markdown).parse(markdownText)
+
+  let titleDepth: number | null = null
+  let foundHeading = false
+  const extractedNodes: typeof tree.children = []
+
+  for(const node of tree.children) {
+    if(foundHeading) {
+      if (node.type === "heading" && node.depth <= titleDepth) {
+        // Stop if an equally or more important heading is found
+        break
+      }
+      extractedNodes.push(node)
+    } else {
+      if (node.type === "heading") {
+          foundHeading = true
+          titleDepth = node.depth
+      }
+    }
+  }
+
+  return extractedNodes.map(node => remark().stringify(node)).join("\n")
+}
+
 
 type SimpleSummaryProps = {
   content: string
 }
 
 const SimpleSummary = ({ content }: SimpleSummaryProps) => {
-  const simpleSummaryText = useMemo(() => {
-    return extractParagraphByTitle(content, "Simple Summary") || content
+
+  const extractedData = useMemo(() => {
+    const simpleSummaryContent = extractParagraphByTitle(content, "Simple Summary")
+    if (simpleSummaryContent)  {
+      return { title: "Simple Summary", content: simpleSummaryContent }
+    }
+
+    const summaryContent = extractParagraphByTitle(content, "Summary")
+    if (summaryContent)  {
+      return { title: "Summary", content: summaryContent }
+    }
+
+    const firstSectionContent = extractFirstSection(content)
+    if (firstSectionContent) {
+      return { title: null, content: firstSectionContent }
+    }
+
   }, [content])
 
-  return <ReactMarkdown
-    skipHtml={true}
-    remarkPlugins={[remarkGfm]}
-    linkTarget="_blank"
-    className="react-markdown"
-  >
-    {simpleSummaryText}
-  </ReactMarkdown>
+  if (extractedData) {
+    return <React.Fragment>
+      {extractedData.title !== null && <h3>{extractedData.title}</h3>}
+      <Box sx={{mt: [2]}}>
+        <ReactMarkdown
+          skipHtml={true}
+          remarkPlugins={[remarkGfm]}
+          linkTarget="_blank"
+          className="react-markdown"
+        >
+          {extractedData.content}
+        </ReactMarkdown>
+      </Box>
+    </React.Fragment>
+  } else {
+    return <React.Fragment>
+      <ReactMarkdown
+        skipHtml={true}
+        remarkPlugins={[remarkGfm]}
+        linkTarget="_blank"
+        className="react-markdown"
+      >
+        {content}
+      </ReactMarkdown>
+    </React.Fragment>
+  }
 }
 
 export default SimpleSummary
