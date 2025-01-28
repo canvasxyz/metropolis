ff7feba30719c37bc45c9db9b635ae0490b6c81f
Bob Webb <rjwebb@users.noreply.github.com>
2025-01-28 18:56:49 +0100

    remove `GET /api/v3/conversations` endpoint (#81)
    * use get single conversation endpoint for report
    
    * remove unused conversations store
    
    * remove /api/v3/conversations endpoint
    
    * add ownername to conversation/:id endpoint

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,7 +1,6 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { combineReducers } from "redux"
-import conversations from "./conversations"
 import user from "./user"
 import zid_metadata from "./zid_metadata"
 import mod_comments_accepted from "./mod_comments_accepted"
@@ -22,7 +21,6 @@ import conversation_voters from "./voters"
 import view_counts from "./view_counts"
 
 const rootReducer = combineReducers({
-  conversations,
   user,
   zid_metadata,
   view_counts,
